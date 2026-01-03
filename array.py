050ee4b3e00ce5791a5c94d54ac2735175dcc091
kamblearpita322-cloud <kamblearpita322@gmail.com>
2026-01-03 09:41:34 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,19 @@
+def main():
+
+scores = [78, 85, 90, 66, 88]
+
+total = sum(scores)
+
+average = total / len(scores)
+
+print("=== main/master branch output ===")
+
+print(f"Scores: {scores}")
+
+print(f"Sum: {total}")
+
+print(f"Average: {average}")
+
+if__name__ = "__main__":
+
+main()
